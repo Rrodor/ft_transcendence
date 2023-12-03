fca782f94a5b66d8bc838e5e94de61ad6cf8d5c7
Rrodor <romeo.rodor@gmail.com>
2023-12-03 18:08:22 +0100

    add archi p2

--- transcendence/transcendence/settings.py
+++ transcendence/transcendence/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!@)u(du1+73n(-)!(fese1fz8z4rrgs$m6tbap&gr9q&r78ul1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -119,7 +119,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 #STATIC_ROOT  = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = "/home/romeo/Documents/programmation/42/baseprojects/ft_transcendence/transcendence/static/"
+#STATIC_ROOT = "/home/romeo/Documents/programmation/42/baseprojects/ft_transcendence/transcendence/static/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
