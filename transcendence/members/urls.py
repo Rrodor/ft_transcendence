0e014e8b3818b874267597f6eb02a45b84481ee7
BaBreton <baptiste.br@icloud.com>
2023-12-18 19:46:10 +0100

    add front and bootstrap

--- transcendence/members/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-		path('', views.main, name='main'),
-        path('players/', views.players, name='players'),
-        path('players/details/<int:id>', views.details, name='details'),
-		path('testing/', views.testing, name='testing'),
-]
