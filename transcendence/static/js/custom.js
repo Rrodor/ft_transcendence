5af5ac36d03ab44c3320182c33bf680fb46ee822
BaBreton <baptiste.br@icloud.com>
2024-01-13 16:46:04 +0100

    fix ugly menu with languages selector

--- transcendence/static/js/custom.js
+++ transcendence/static/js/custom.js
@@ -64,4 +64,7 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 });
 
-
+function changeLanguage(languageCode) {
+    document.getElementById('language-input').value = languageCode;
+    document.getElementById('language-form').submit();
+}
